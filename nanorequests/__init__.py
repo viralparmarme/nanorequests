fba85f751b37bd3eb2b7424c5fff2d59e8fdf17a
Viral Parmar <viralparmarme@gmail.com>
2023-07-01 13:16:49 +0530

    File restructuring and config updates

---
+++ nanorequests/__init__.py
@@ -0,0 +1,1 @@
+from nanorequests import *
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nanorequests',
-    version='0.8',
+    version='0.9',
     license='MIT',
     author="Viral Parmar",
     description='The simplest REST requests package for quick API integration',
@@ -12,6 +12,8 @@ setup(
     url='https://github.com/viralparmarme/nanorequests',
     keywords='nanorequests viralparmarme viralparmar',
     py_modules=['nanorequests', 'exceptions'],
+    long_description="README.md",
+    long_description_content_type="text/markdown",
     install_requires=[
         'requests'
     ],

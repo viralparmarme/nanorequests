60ded74ec6353612c4b8157170bc11f42b219278
Viral Parmar <viralparmarme@gmail.com>
2023-07-01 12:49:49 +0530

    Update current packages config

--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ setup(
     author="Viral Parmar",
     description='The simplest REST requests package for quick API integration',
     author_email='viralparmarme@gmail.com',
+    packages=find_packages(),
     url='https://github.com/viralparmarme/nanorequests',
     keywords='nanorequests viralparmarme viralparmar',
-    py_modules=['nanorequests', 'nanoexceptions'],
+    py_modules=['nanorequests', 'exceptions'],
     install_requires=[
         'requests'
     ],
